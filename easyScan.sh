764400be9a039301c86deb0c04616c2f07dc1bea
unracer <59308598+unracer@users.noreply.github.com>
2022-03-07 15:04:06 +0000

    Add files via upload

---
+++ easyScan.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "easyScan.sh -i <ip> -d <domain>" $(date +"%T") ;
+while getopts i:d: flag
+do
+    case "${flag}" in
+        i) ip=${OPTARG};;
+        d) domain=${OPTARG};;
+    esac
+done
+
+    sudo echo $ip $domain >> /etc/hosts;
+    echo "\033[32mPORTS\033[00m";
+    nmap -p- -T5 --min-rate 2000 -n $domain;
+    sudo nmap -T5 -n -sU -p- --min-rate 2000 $domain;
+    echo "\033[32mDIRS\033[00m";
+    dirsearch -u $domain -t 256 -w /home/kali/Downloads/enum/directory-list-lowercase-2.3-medium.txt;
+    echo "\033[32mDOMAIN\033[00m";
+    gobuster vhost --url $domain -w /home/kali/Downloads/enum/subdomains-top1million-110000.txt -t 64;
+
+    echo "\033[32mfinish\033[00m" $(date +"%T");
